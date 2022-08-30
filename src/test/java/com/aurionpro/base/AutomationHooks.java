5cb33bd5196a8e745c8408be0a6cbcc2971c7eb2
balaji-githubstore <31404824+balaji-githubstore@users.noreply.github.com>
2022-08-30 13:17:07 +0530

    added comment in hooks class

--- src/test/java/com/aurionpro/base/AutomationHooks.java
+++ src/test/java/com/aurionpro/base/AutomationHooks.java
@@ -3,10 +3,15 @@ package com.aurionpro.base;
 import org.openqa.selenium.WebDriver;
 
 import io.cucumber.java.After;
-
+/**
+ * 
+ * @author Balaji
+ *
+ */
 public class AutomationHooks {
 	public static WebDriver driver;
 		
+	
 	
 	@After
 	public void endScenario()
